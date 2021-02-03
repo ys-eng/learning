571b9ab289da0e84165848a10ec6d565d470ac4b
yamamoto <shanbenjunjie@gmail.com>
2021-02-03 20:47:08 +0900

    calculatorファイルをlearningファイル内に移動

---
+++ calculator/index.ts
@@ -0,0 +1,36 @@
+document.getElementById("button-0").onclick = function() {
+    document.getElementById("button-result").innerHTML = "0";
+};
+document.getElementById("button-1").onclick = function() {
+    document.getElementById("button-result").innerHTML = "1";
+};
+document.getElementById("button-2").onclick = function() {
+    document.getElementById("button-result").innerHTML = "2";
+};
+document.getElementById("button-3").onclick = function() {
+    document.getElementById("button-result").innerHTML = "3";
+};
+document.getElementById("button-4").onclick = function() {
+    document.getElementById("button-result").innerHTML = "4";
+};
+document.getElementById("button-5").onclick = function() {
+    document.getElementById("button-result").innerHTML = "5";
+};
+document.getElementById("button-6").onclick = function() {
+    document.getElementById("button-result").innerHTML = "6";
+};
+document.getElementById("button-7").onclick = function() {
+    document.getElementById("button-result").innerHTML = "7";
+};
+document.getElementById("button-8").onclick = function() {
+    document.getElementById("button-result").innerHTML = "8";
+};
+document.getElementById("button-9").onclick = function() {
+    document.getElementById("button-result").innerHTML = "9";
+};
+document.getElementById("button-+").onclick = function() {
+    document.getElementById("button-result").innerHTML = "+";
+};
+document.getElementById("button-c").onclick = function() {
+    document.getElementById("button-result").innerHTML = "0";
+};
