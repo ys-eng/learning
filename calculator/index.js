d1c312a6fffa9b9c0f98ea55092eb8e806b63f9a
Yuta Shimogishi <techreis417@gmail.com>
2021-02-03 22:28:23 +0900

    index.tsをコンパイルした

--- calculator/index.js
+++ calculator/index.js
@@ -1,36 +1,47 @@
-document.getElementById("button-0").onclick = function () {
-    document.getElementById("button-result").innerHTML = "0";
+var btns = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    // "+",
+    "c",
+];
+var calcArray = [];
+var _loop_1 = function (item) {
+    var elementId = item;
+    var value = item;
+    if (item === "c") {
+        value = "0";
+    }
+    var btnElement = (document.getElementById("button-" + elementId));
+    btnElement.onclick = function () {
+        calcArray.push(value);
+        document.getElementById("button-result").innerHTML = value;
+        console.log(JSON.stringify(calcArray));
+    };
 };
-document.getElementById("button-1").onclick = function () {
-    document.getElementById("button-result").innerHTML = "1";
-};
-document.getElementById("button-2").onclick = function () {
-    document.getElementById("button-result").innerHTML = "2";
-};
-document.getElementById("button-3").onclick = function () {
-    document.getElementById("button-result").innerHTML = "3";
-};
-document.getElementById("button-4").onclick = function () {
-    document.getElementById("button-result").innerHTML = "4";
-};
-document.getElementById("button-5").onclick = function () {
-    document.getElementById("button-result").innerHTML = "5";
-};
-document.getElementById("button-6").onclick = function () {
-    document.getElementById("button-result").innerHTML = "6";
-};
-document.getElementById("button-7").onclick = function () {
-    document.getElementById("button-result").innerHTML = "7";
-};
-document.getElementById("button-8").onclick = function () {
-    document.getElementById("button-result").innerHTML = "8";
-};
-document.getElementById("button-9").onclick = function () {
-    document.getElementById("button-result").innerHTML = "9";
-};
-document.getElementById("button-+").onclick = function () {
+for (var _i = 0, btns_1 = btns; _i < btns_1.length; _i++) {
+    var item = btns_1[_i];
+    _loop_1(item);
+}
+// プラスボタン
+var btnPlus = (document.getElementById("button-plus"));
+btnPlus.onclick = function () {
+    calcArray.push("+");
     document.getElementById("button-result").innerHTML = "+";
+    console.log(JSON.stringify(calcArray));
 };
-document.getElementById("button-c").onclick = function () {
-    document.getElementById("button-result").innerHTML = "0";
+// イコールボタン
+var btnEq = document.getElementById("button-eq");
+btnEq.onclick = function () {
+    calcArray.push("=");
+    var reuslt = calcArray.join(" ");
+    document.getElementById("button-result").innerHTML = reuslt;
+    console.log(JSON.stringify(calcArray));
 };
