f553fd44669d6a72ec78097fb810359d35a50863
yamamoto <shanbenjunjie@gmail.com>
2021-02-16 21:42:50 +0900

    TSでmoment.jsを使い、時計の針を１秒ごとに動かす処理を実装

--- analog-clock/src/index.js
+++ analog-clock/src/index.js
@@ -1,8 +1,7 @@
 "use strict";
 exports.__esModule = true;
 var moment_1 = require("moment");
-// const btn: HTMLElement = <HTMLElement>document.getElementById("sample");
-// btn.onclick = () => {
-//   console.log(moment().format("YYYY-MM-DD"));
-// };
-console.log('Moment=' + moment_1["default"]().format('LLLL'));
+var btn = document.getElementById("sample");
+btn.onclick = function () {
+    console.log(moment_1["default"]().format("YYYY-MM-DD"));
+};
--- analog-clock/src/index.ts
+++ analog-clock/src/index.ts
@@ -1,5 +1,15 @@
 import moment from "moment";
-const btn: HTMLElement = <HTMLElement>document.getElementById("sample");
-btn.onclick = () => {
-  console.log(moment().format("YYYY-MM-DD"));
-};
+
+setInterval(() => {
+  const H: number = Number(moment().format("h"));
+  const M: number = Number(moment().format("mm"));
+  const S: number = Number(moment().format("ss"));
+
+  const Hour: HTMLElement = <HTMLElement>document.getElementById("hour");
+  const Minute: HTMLElement = <HTMLElement>document.getElementById("minute");
+  const Second: HTMLElement = <HTMLElement>document.getElementById("second");
+
+  Hour.style.transform = `rotate(${H*30+M/2}deg)`;
+  Minute.style.transform = `rotate(${M*6+S/10}deg)`;
+  Second.style.transform = `rotate(${S*6}deg)`;
+});
