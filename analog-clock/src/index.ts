98418c28c1d539cfc8f0f00bae7da7d1923d289b
yamamoto <shanbenjunjie@gmail.com>
2021-02-18 11:04:59 +0900

    三項演算子を使い、ゼロ除算の解消

--- analog-clock/src/index.ts
+++ analog-clock/src/index.ts
@@ -12,9 +12,14 @@ const intervalId = setInterval(() => {
   const millisecond: number = now.milliseconds();
 
   // 時針は1時間で360/12度、1分間で360/12/60度、1秒間で360/12/60/60度動く
-  hourHand.style.transform = `rotate(${hour * 30 / 12 + minute * 360 / 12 / 60}deg)`;
+  const deltaMinultes = minute === 0 ? 0 : minute * 360 / 12 / 60;
+  hourHand.style.transform = `rotate(${hour * 30 + deltaMinultes}deg)`;
+
   // 分針は1時間で360度、1分間で360/60度、1秒間で360/60/60度動く
-  minuteHand.style.transform = `rotate(${minute * 6 + second * 360 / 60 / 60}deg)`;
+  const deltaSeconds = second === 0 ? 0 : second * 360 / 60 / 60;
+  minuteHand.style.transform = `rotate(${minute * 6 + deltaSeconds}deg)`;
+
   // 秒針は1分間で360度、1秒間で360/60度動く、1ミリ秒で360/60/1000度動く
-  secondeHand.style.transform = `rotate(${second * 6 + millisecond * 360 / 60 / 1000}deg)`;
-},10)
+  const deltaMillisecond = millisecond === 0 ? 0 : millisecond * 360 /60 / 1000;
+  secondeHand.style.transform = `rotate(${second * 6 + deltaMillisecond}deg)`;
+},10);
